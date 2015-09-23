037e70dbd87a0e6061bbe1cecc3d94b18cb26844
Jan Adrianus Germeraad <jarinusgermeraad@hotmail.com>
2015-09-23 11:35:25 +0200

    Added basic XML parser and Server

---
+++ Leertaak 1/src/nl/jgermeraad/Server.java
@@ -0,0 +1,30 @@
+package nl.jgermeraad;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+	public static final int SOCKET = 7789;
+	public static final DatabaseHandler databaseHandler = new DatabaseHandler();
+	
+	public static void main(String[] args) {
+		ServerSocket socket = null;
+		try {
+			socket = new ServerSocket(SOCKET);
+			Socket connection = null;
+			while(true) {
+				if((connection = socket.accept()) != null) {
+					new IncomingDataHandler(connection).run();
+				}
+			}
+		} catch (Exception e) {
+			System.err.println(e);
+		} finally {
+			try {
+				socket.close();
+			} catch (Exception e) {
+				System.err.println(e);
+			}
+		}
+	}
+}
